523ea3c8ee2a27787c6df55ad3755ea6776c7ad4
Ezequiel Geringer <ezequielgeringer@gmail.com>
2022-08-18 10:52:26 -0300

    BUG-1: Fix misspelling

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Verssion 0.0.1
+          Version 0.0.1
         </p>
         <a
           className="App-link"
